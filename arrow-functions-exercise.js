a5e0e8023b74ba57f7d2a0c5e6c4f92db30b6698
Marcus Garnot <marcusgarnot@Marcuss-Air.lan>
2023-11-30 17:15:59 -0500

    finished file

---
+++ arrow-functions-exercise.js
@@ -0,0 +1,9 @@
+/* Write an ES2015 Version */
+
+    const double = (arr) => arr.map((val) => (val * 2));
+
+/* Replace ALL functions with arrow functions: */
+
+const squareAndFindEvens = (numbers) =>  numbers.map((num) => num ** 2).filter((square) => square % 2 === 0)
+
+
